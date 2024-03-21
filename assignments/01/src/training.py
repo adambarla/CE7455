b5b149540aa50751c94ec11d6bbcc3f21d911848
Adam Barla <barlaada@fit.cvut.cz>
2024-03-21 12:40:04 +0800

    refactor of utils

--- assignments/01/src/training.py
+++ assignments/01/src/training.py
@@ -1,47 +1,10 @@
-from datetime import datetime
 import wandb
 from tqdm import tqdm
 import hydra
-import numpy as np
-import random
 from omegaconf import OmegaConf, DictConfig
 from torchtext import data
 import torch
-from torchtext import datasets
-
-
-def load_data(seed):
-    # For tokenization
-    TEXT = data.Field(
-        tokenize="spacy", tokenizer_language="en_core_web_sm", include_lengths=True
-    )
-
-    # For multi-class classification labels
-    LABEL = data.LabelField()
-
-    # Load the TREC dataset
-    train_data, test_data = datasets.TREC.splits(TEXT, LABEL, fine_grained=False)
-    train_data, valid_data = train_data.split(
-        split_ratio=0.8, random_state=random.seed(seed)
-    )
-
-    print(f"Number of training examples: {len(train_data)}")
-    print(f"Number of validation examples: {len(valid_data)}")
-    print(f"Number of testing examples: {len(test_data)}")
-
-    TEXT.build_vocab(train_data, max_size=10000)
-    LABEL.build_vocab(train_data)
-
-    print(f"Unique tokens in TEXT vocabulary: {len(TEXT.vocab)}")
-    print(f"Unique tokens in LABEL vocabulary: {len(LABEL.vocab)}")
-    return train_data, valid_data, test_data, TEXT, LABEL
-
-
-def set_deterministic(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+from utils import get_device, init_run, load_data, set_deterministic
 
 
 def epoch_train(model, iterator, optimizer, criterion):
@@ -120,36 +83,6 @@ def train(model, optimizer, criterion, n_epochs, train_iterator, valid_iterator)
                     "valid_acc": valid_acc,
                 }
             )
-
-
-def get_device(cfg: DictConfig) -> torch.device:
-    if cfg.device is not None:
-        device = torch.device(cfg.device)
-    else:
-        device = torch.device(
-            "cuda"
-            if torch.cuda.is_available()
-            else "mps"
-            if torch.backends.mps.is_available()
-            else "cpu"
-        )
-    return device
-
-
-def init_run(cfg):
-    if cfg.name is None:
-        m = cfg.model._target_.split(".")[-1]
-        o = cfg.optimizer._target_.split(".")[-1]
-        t = datetime.now().strftime("%Y%m%d_%H%M%S")
-        cfg.name = f"{m}_{o}_{t}"
-    wandb.init(
-        name=cfg.name,
-        group=cfg.group,
-        project=cfg.wandb.project,
-        entity=cfg.wandb.entity,
-        config=OmegaConf.to_container(cfg, resolve=True),
-        reinit=True,
-    )
 
 
 @hydra.main(config_path="conf", config_name="main", version_base=None)
--- assignments/01/src/utils.py
+++ assignments/01/src/utils.py
@@ -1,2 +1,77 @@
+from datetime import datetime
+from torchtext import data
+import numpy as np
+import random
+
+import torch
+import wandb
+from torchtext import datasets
+from omegaconf import OmegaConf, DictConfig
+
+
 def is_sorted(sequence):
     return all(sequence[i] >= sequence[i + 1] for i in range(len(sequence) - 1))
+
+
+def get_device(cfg: DictConfig) -> torch.device:
+    if cfg.device is not None:
+        device = torch.device(cfg.device)
+    else:
+        device = torch.device(
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+    return device
+
+
+def init_run(cfg):
+    if cfg.name is None:
+        m = cfg.model._target_.split(".")[-1]
+        o = cfg.optimizer._target_.split(".")[-1]
+        t = datetime.now().strftime("%Y%m%d_%H%M%S")
+        cfg.name = f"{m}_{o}_{t}"
+    wandb.init(
+        name=cfg.name,
+        group=cfg.group,
+        project=cfg.wandb.project,
+        entity=cfg.wandb.entity,
+        config=OmegaConf.to_container(cfg, resolve=True),
+        reinit=True,
+    )
+
+
+def load_data(seed):
+    # For tokenization
+    TEXT = data.Field(
+        tokenize="spacy", tokenizer_language="en_core_web_sm", include_lengths=True
+    )
+
+    # For multi-class classification labels
+    LABEL = data.LabelField()
+
+    # Load the TREC dataset
+    train_data, test_data = datasets.TREC.splits(TEXT, LABEL, fine_grained=False)
+    train_data, valid_data = train_data.split(
+        split_ratio=0.8, random_state=random.seed(seed)
+    )
+
+    print(f"Number of training examples: {len(train_data)}")
+    print(f"Number of validation examples: {len(valid_data)}")
+    print(f"Number of testing examples: {len(test_data)}")
+
+    TEXT.build_vocab(train_data, max_size=10000)
+    LABEL.build_vocab(train_data)
+
+    print(f"Unique tokens in TEXT vocabulary: {len(TEXT.vocab)}")
+    print(f"Unique tokens in LABEL vocabulary: {len(LABEL.vocab)}")
+    return train_data, valid_data, test_data, TEXT, LABEL
+
+
+def set_deterministic(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
