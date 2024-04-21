8ea3cb891f22b1dbfd6cc152180bd846d8a795fa
Adam Barla <barlaada@fit.cvut.cz>
2024-04-21 21:21:59 +0800

    refactor: removed old training script and unused methods

--- assignments/02/src/utils/misc.py
+++ assignments/02/src/utils/misc.py
@@ -4,22 +4,6 @@ import random
 import torch
 import wandb
 from omegaconf import OmegaConf, DictConfig
-import time
-import math
-
-
-def as_minutes(s):
-    m = math.floor(s / 60)
-    s -= m * 60
-    return '%dm %ds' % (m, s)
-
-
-def time_since(since, percent):
-    now = time.time()
-    s = now - since
-    es = s / percent
-    rs = es - s
-    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
 
 
 def get_device(cfg: DictConfig) -> torch.device:
@@ -51,22 +35,6 @@ def init_run(cfg):
         config=OmegaConf.to_container(cfg, resolve=True),
         reinit=True,
     )
-
-
-def indexes_from_sentence(lang, sentence):
-    return [lang.word2index[word] for word in sentence.split(' ')]
-
-
-def tensor_from_sentence(lang, sentence, device, eos_token):
-    indexes = indexes_from_sentence(lang, sentence)
-    indexes.append(eos_token)
-    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
-
-
-def tensors_from_pair(pair, input_lang, output_lang, device, eos_token):
-    input_tensor = tensor_from_sentence(input_lang, pair[0], device, eos_token)
-    target_tensor = tensor_from_sentence(output_lang, pair[1], device, eos_token)
-    return input_tensor, target_tensor
 
 
 def set_deterministic(seed):
